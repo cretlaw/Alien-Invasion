94428628bdf3cf61f3f2eebd2af943ecae006c58
winterMute84 <wcarbajal84@gmail.com>
2019-01-30 23:05:55 -0800

    Added range left and right so that ship does not go off the screen

--- game_functions.py
+++ game_functions.py
@@ -7,18 +7,10 @@ def check_events(ship):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
-
+            check_Keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
 
 
 def update_screen(ai_settings, screen, ship):
@@ -29,3 +21,19 @@ def update_screen(ai_settings, screen, ship):
 
     # Make the most recently drawn screen visible
     pygame.display.flip()
+
+
+def check_Keydown_events(event, ship):
+    '''Responses to keypresses'''
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    '''Respond to key releases'''
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
