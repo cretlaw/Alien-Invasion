886a62efeb0e3012a489a3c273cf6e5e8d5128fe
winterMute84 <wcarbajal84@gmail.com>
2019-01-31 00:42:54 -0800

    added bullets made sure they were destroyed when they reached the top

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 
 
 def run_game():
@@ -18,12 +19,16 @@ def run_game():
 
     # make a ship
     ship = Ship(ai_settings, screen)
+
+    # make a group to store bullets in
+    bullets = Group()
     # Start the main loop for the game.
     while True:
         # Watch for keyboard and mouse events.
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
