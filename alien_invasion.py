f7a920f041db9fc973416097d5e1003f1d425742
winterMute84 <wcarbajal84@gmail.com>
2019-01-30 17:50:15 -0800

    Set up playscreen and overlayed space ship on it.

---
+++ alien_invasion.py
@@ -0,0 +1,28 @@
+import pygame
+import sys
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+
+def run_game():
+    # Initialize game and create a screen object.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # set the background color
+    bg_color = (230, 230, 230)
+
+    # make a ship
+    ship = Ship(screen)
+    # Start the main loop for the game.
+    while True:
+        # Watch for keyboard and mouse events.
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
+
+
+run_game()
