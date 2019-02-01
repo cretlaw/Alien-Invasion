d2c3c060288d51373c52ab7bc953665b5fbd8911
winterMute84 <wcarbajal84@gmail.com>
2019-02-01 15:19:25 -0800

    implemented players lives GUI

--- alien_invasion.py
+++ alien_invasion.py
@@ -45,7 +45,8 @@ def run_game():
             gf.update_bullets(ai_settings, screen, stats,
                               sb, ship, aliens, bullets)
 
-            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, sb,
+                             screen, ship, aliens, bullets)
 
         gf.update_screen(ai_settings, screen, stats, sb, ship,
                          aliens, bullets, play_button)
--- ship.py
+++ ship.py
@@ -1,9 +1,11 @@
 import pygame
+from pygame.sprite import Sprite
 
 
-class Ship():
+class Ship(Sprite):
     def __init__(self, ai_setttings, screen):
         '''Initialize the ship and starting position'''
+        super(Ship, self).__init__()
         self.screen = screen
         self.ai_setttings = ai_setttings
 
