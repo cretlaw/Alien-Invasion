ec64dff7001698071ea60b2acbf1c065a9079ea3
winterMute84 <wcarbajal84@gmail.com>
2019-01-31 16:49:29 -0800

    created mutlitple rows of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,7 +23,7 @@ def run_game():
     aliens = Group()
 
     # create the fleet of aliens
-    gf.create_fleet(ai_settings, screen, aliens)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
     # Start the main loop for the game.
     while True:
         # Watch for keyboard and mouse events.
